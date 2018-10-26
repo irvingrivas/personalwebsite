6df8f79ae227ec9c8f4e0368d5041a76a61f6507
irvingrivas <irving0594@gmail.com>
2018-10-25 21:17:41 -0700

    Cleaned up Client Side Javascript

--- assets/javascript/readmsg.js
+++ assets/javascript/readmsg.js
@@ -1,9 +1,9 @@
-$("#send-emails").on("click", function() {
-    var email_message = {
-        email: $("#user-email").val().trim(),
-        message: $("#user-message").val().trim()
-    };
-    $.post("/",email_message)
-        .then(function(data) {
+$(document).ready(function(){
+    $("#send-emails").on("click", function() {
+        var email_message = {
+            email: $("#user-email").val().trim(),
+            message: $("#user-message").val().trim()
+        };
+        $.post("/",email_message);
     });
 });
