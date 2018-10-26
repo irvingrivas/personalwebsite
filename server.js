06ff7b62911d5e7ea66f67f9c7056bc2d43f649b
irvingrivas <irving0594@gmail.com>
2018-10-26 08:31:14 -0700

    Finished nice email

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require("path");
 const keys = require("./keys.js");
-const fs = require("fs");
 const PORT = process.env.PORT || 8080;
 
 const app = express();
