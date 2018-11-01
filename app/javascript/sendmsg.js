ed9dd05bb0158b92cacc992c7add2141db71e691
irvingrivas <irving0594@gmail.com>
2018-11-01 02:47:57 -0700

    Fixed Major State Issue

--- app/javascript/sendmsg.js
+++ app/javascript/sendmsg.js
@@ -1,14 +1,15 @@
 $(document).ready(function () {
-    $("#send-emails").on("click", function () {
+    $("#send-emails").on("click", function (e) {
+        e.preventDefault();
         var email_message = {
             email: $("#user-email").val().trim(),
             message: $("#user-message").val().trim(),
             captcha: $("#g-recaptcha-response").val().trim()
         };
-        $.post("/reply", email_message)
-            .success(function () {
-                console.log("Changing URL");
-                document.location.href = "/reply";
-            });
+        $.post("/reply", email_message, function(data) {
+            $("#form-res").text(data.msg);
+            //jQuery.noConflict(); // dirty
+            $("#response-modal").modal("show");
+        });
     });
 });
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const nodemailer = require('nodemailer');
 const express = require('express');
-const exphbs = require("express-handlebars");
 const bodyParser = require('body-parser');
 const path = require("path");
 const keys = require("./keys.js");
@@ -13,17 +12,9 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "app")));
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "app/views/index.html"));
-});
-
-app.get("/reply", function (req, res) {
-  if (app.locals.msg === undefined) {
-    app.locals.msg = "Your message was not sent. Please input contact info."
-  } return res.render("reply", { msg: app.locals.msg });
 });
 
 app.post("/reply", function (req, res) {
@@ -31,15 +22,14 @@ app.post("/reply", function (req, res) {
   if (req.body.captcha === undefined ||
     req.body.captcha === '' ||
     req.body.captcha === null) {
-    return app.locals.msg = "Your message was not sent. Failed captcha verification.";
-  } else if (req.body.email === "" && !(req.body.message === "")) {
-    return app.locals.msg = "Your message was not sent. Check email address.";
-  } else if (!(req.body.email === "") && req.body.message === "") {
-    return app.locals.msg = "Your message was not sent. Check message entry.";
-  } else if (req.body.email === "" && req.body.message === "") {
-    return app.locals.msg = "Your message was not sent. Check contact info.";
+    return res.json({msg: "Your message was not sent. Please validate captcha."})
+  } else if (
+    req.body.email === undefined ||
+    req.body.email === '' ||
+    req.body.email === null) {
+    return res.json({msg: "Your message was not sent. Please input contact info."})
   } else {
-    app.locals.msg = "Your message was sent!";
+    msg = "Your message was sent!";
   }
 
   var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + keys.gmailinfo.CAPTCHASECRETKEY + "&response=" + req.body.captcha + "&remoteip=" + req.connection.remoteAddress;
@@ -48,7 +38,7 @@ app.post("/reply", function (req, res) {
     body = JSON.parse(body);
     // Success will be true or false depending upon captcha validation.
     if (body.success !== undefined && !body.success) {
-      return app.locals.msg = "Your message was not sent. Invalid Captcha.";
+      return res.json({msg: "Your message was not sent. Invalid Captcha."});
     }
     let mailOptsToServer, mailOptsToClient, smtpTrans;
     var emailcontent = "";
@@ -76,7 +66,7 @@ app.post("/reply", function (req, res) {
         text: req.body.message
       },
         smtpTrans.sendMail(mailOptsToServer, function (error) {
-          if (error) throw err;
+          if (err) throw err;
           mailOptsToClient = {
             from: keys.gmailinfo.USEREMAIL,
             to: req.body.email,
@@ -85,6 +75,7 @@ app.post("/reply", function (req, res) {
           },
             smtpTrans.sendMail(mailOptsToClient, function (error) {
               if (error) throw error;
+              return res.json({msg: msg});
             });
         });
     });
