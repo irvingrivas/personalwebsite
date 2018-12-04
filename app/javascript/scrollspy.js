85434dd1fba633287b3857aa96658254b807a048
irvingrivas <irving0594@gmail.com>
2018-12-03 23:22:11 -0700

    Added slow scroll on load

--- app/javascript/scrollspy.js
+++ app/javascript/scrollspy.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+    $("html, body").animate({
+        scrollTop: $("#about-page").offset().top + 5
+    }, 1000, "easeInOutQuad");
     $(".page-control").click(function(event){ 
         divId = $(this).attr("href"); 
         event.preventDefault(); 
