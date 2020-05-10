b77e83bef5167c9cb22f7dd9ab7866c58c69ab2f
irvingrivas <irving0594@gmail.com>
2020-05-10 03:13:32 -0400

    Modernizing Code

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 require("dotenv").config();
 const nodemailer = require('nodemailer');
-const express = require('express');
+const express    = require('express');
 const bodyParser = require('body-parser')
-const path = require("path");
-const keys = require("./keys.js");
-const request = require("request");
-const fs = require("fs");
-const PORT = process.env.PORT || 8080;
-const app = express();
+const path       = require("path");
+const keys       = require("./keys.js");
+const request    = require("request");
+const fs         = require("fs");
+const PORT       = process.env.PORT || 8080;
+const app        = express();
 
 app.use(express.static(path.join(__dirname, "app")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "app/views/index.html"));
 });
 
-app.post("/reply", function (req, res) {
+app.post("/reply", (req, res) => {
+
   // if its blank or null means user has not selected the captcha, so return the error.
   if (req.body.captcha === undefined ||
     req.body.captcha === '' ||
@@ -30,60 +31,64 @@ app.post("/reply", function (req, res) {
     return res.json({ msg: "Your message was not sent. Please input contact info." })
   }
 
-  var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + keys.gmailinfo.CAPTCHASECRETKEY + "&response=" + req.body.captcha + "&remoteip=" + req.connection.remoteAddress;
-  // Hitting GET request to the URL, Google will respond with success or error scenario.
-  request(verificationUrl, function (err, result, body) {
-    // Success will be true or false depending upon captcha validation.
-    if (body.success !== undefined && !body.success) {
-      return res.json({ msg: "Your message was not sent. Invalid Captcha." });
+  // Hitting GET request to the Verification URL.
+  var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + 
+    keys.gmailinfo.CAPTCHASECRETKEY + "&response=" + req.body.captcha + 
+    "&remoteip=" + req.connection.remoteAddress;
+  
+  request(verificationUrl, (err) => {
+    if (err) return res.json({ msg: "Your message was not sent. Invalid Captcha." });
+  });
+
+  // Get email content from user message
+  let emailContent = req.body.message;
+  fs.readFileSync("app/views/response.html", (err, data) => {
+    if (err) throw err;
+    emailContent = data + emailContent;
+  });
+
+  // Establish SMPT Transport
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      type: 'OAuth2',
+      user: keys.gmailinfo.NOREPLYEMAIL,
+      clientId: keys.gmailinfo.CLIENTID,
+      clientSecret: keys.gmailinfo.CLIENTSECRET,
+      refreshToken: keys.gmailinfo.REFRESHTOKEN,
+      accessToken: keys.gmailinfo.ACCESSTOKEN
     }
-    let mailOptsToServer, mailOptsToClient, smtpTrans;
-    let emailcontent = "";
-    fs.readFile("app/views/response.html", function (err, data) {
-      if (err) throw err;
-      emailcontent = data;
-      emailcontent += req.body.message;
-      smtpTrans = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-          type: 'OAuth2',
-          user: keys.gmailinfo.NOREPLYEMAIL,
-          clientId: keys.gmailinfo.CLIENTID,
-          clientSecret: keys.gmailinfo.CLIENTSECRET,
-          refreshToken: keys.gmailinfo.REFRESHTOKEN,
-          accessToken: keys.gmailinfo.ACCESSTOKEN
-        }
-      });
-      mailOptsToClient = {
-        from: keys.gmailinfo.NOREPLYEMAIL,
-        to: req.body.email,
-        subject: "Thank You For Contacting Irving Rivas",
-        html: emailcontent
-      },
-        smtpTrans.sendMail(mailOptsToClient, function (err) {
-          if (err) {
-            return res.json({ msg: "Your message was not sent. Email entered cannot be reached!"});
-          }
-          mailOptsToServer = {
-            from: keys.gmailinfo.NOREPLYEMAIL,
-            to: keys.gmailinfo.PERSONALEMAIL,
-            subject: "New message from " + req.body.email + " @ irvingrivas.com",
-            text: req.body.message
-          },
-            smtpTrans.sendMail(mailOptsToServer, function () {
-              return res.json({ msg: "Your message was sent! Please check your inbox for confirmation." });
-            });
-        });
-    });
+  });
+
+  // Send mail to User
+  transporter.sendMail({
+    from: keys.gmailinfo.NOREPLYEMAIL,
+    to: req.body.email,
+    subject: "Thank You For Contacting Irving Rivas",
+    html: emailcontent
+  }, (err) => {
+    if (err) return res.json({ msg: "Your message was not sent. " +
+      "Please check your email entry on form." });
+  });
+
+  // Send mail to me 
+  transporter.sendMail({
+    from: keys.gmailinfo.NOREPLYEMAIL,
+    to: keys.gmailinfo.PERSONALEMAIL,
+    subject: "New message from " + req.body.email + " @ irvingrivas.com",
+    text: req.body.message
+  }, () => {
+      return res.json({ msg: "Your message was sent! " +
+        "Please check your inbox for confirmation." });
   });
 });
 
-app.get("*", function (req, res) {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "app/views/index.html"));
 });
 
-app.listen(PORT, function () {
-  console.log("This is running on PORT: " + PORT);
+app.listen(PORT, () => {
+  console.log("This is running on http://localhost:" + PORT);
 });
